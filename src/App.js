2887d51436a526d4d7b891cfb27a0fc5177e010a
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-06-13 16:18:50 -0500

    No frills react

---
+++ src/App.js
@@ -0,0 +1,35 @@
+const Pet = (props) => {
+    return (
+        React.createElement("div",{},[
+            React.createElement("h1",{},props.name),
+            React.createElement("h2",{},props.animal),
+            React.createElement("h2",{},props.breed),
+        ])
+    )
+}
+
+
+const App= () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1",{id:"brand"},"Adopt Me!"),
+        React.createElement(Pet,{
+            name:"luna",
+            animal:"dog",
+            breed:"havanese"
+        }),
+        React.createElement(Pet,{
+            name:"pepper",
+            animal:"bird",
+            breed:"cockatiel"
+        }),
+        React.createElement(Pet,{
+            name:"doink",
+            animal:"cat",
+            breed:"mix"
+        }),
+    );
+};
+
+ReactDOM.render(React.createElement(App),document.getElementById("root"));
