a88e529088b919fa86fa4330ed02dfd6770472b6
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-06-14 17:44:41 -0500

    jsx configured

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "luna",
-      animal: "dog",
-      breed: "havanese",
-    }),
-    React.createElement(Pet, {
-      name: "pepper",
-      animal: "bird",
-      breed: "cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "doink",
-      animal: "cat",
-      breed: "mix",
-    }),
-  ]);
+  return (
+    <div>
+      <h1> Adopt Me!</h1>
+      <Pet name="luna" animal="dog" breed="havanese" />
+      <Pet name="pepper" animal="bird" breed="cockatiel" />
+      <Pet name="doink" animal="cat" breed="mix" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
